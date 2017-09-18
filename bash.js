e9290a5c8a2ece7e1645e51267f94de22298c054
Shannon Kendall <snkendall@gmail.com>
2017-09-18 16:02:37 -0400

    added video solutions/optimization up to curl

--- bash.js
+++ bash.js
@@ -6,15 +6,25 @@ process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-  var data = data.toString().trim();
+  data = data.toString().trim();
   var cmd = data.split(' ')[0];
-  var args = data.split(' ').slice(1);
-  // console.log(args);
-  return command[cmd](args);
+  var args = data.split(' ').slice(1) ;
+
+  if (command[cmd]){
+    return command[cmd](args);
+  } else {
+    process.stderr.write('Command not found');
+  }
+
 
   process.stdout.write('\nprompt > ');
 
 });
 // console.log(process.stdout.write(Date()));
 //console.log(process.cwd());
+
+function done (output){
+  process.stdout.write(output);
+  process.stdout.write('\nprompt>');
+}
 
--- command.js
+++ command.js
@@ -1,24 +1,22 @@
 var fs = require('fs');
+var request = require('request');
 
-function pwd() {
-        return process.stdout.write(process.cwd());
-} 
-
-function date() {
-        return process.stdout.write(Date());
+function pwd(args, done) {
+        done(process.cwd());
 }
 
-function ls() {
- fs.readdir('.', function(err, files) {
-        if (err) throw err;
-        files.forEach(function(file) {
-         process.stdout.write(file.toString() + "\n");
-        })
-        process.stdout.write("prompt > ");
-      });
+function date(args, done) {
+        done(Date());
 }
 
-function echo(args) {
+function ls(args, done) {
+        fs.readdir('.', function(err, files) {
+                if (err) throw err;
+                done(files.join('\n'))
+        });
+}
+
+function echo(args, done) {
         var extraCommand;
         args.map(function(el){
                 if (el[0] === '$'){
@@ -26,22 +24,62 @@ function echo(args) {
                 }
         });
         if (extraCommand){
-                return process.stdout.write(process.env(extraCommand));
+                done(process.env(extraCommand));
         }
-        return process.stdout.write(args.join(' '));
+        done(args.join(' '));
 }
 
-function cat(args){
+function cat(args, done){
         var file = args[0];
         console.log(file);
-        fs.readFile(file, (err,data) => {
-                        if (err) throw err;
-                        process.stdout.write(data);
+        fs.readFile(file, (err, data) => {
+                if (err) throw err;
+                done(data);
         });
 }
 
-function head(){
-        
+function head(file, done){
+        // count newlines to print the first five lines
+        fs.readFile(file, function (err, data){
+                if (err) throw err;
+                done(data.split('\n').slice(0, 5).join('\n'));
+        })
+}
+
+function tail(file, done){
+        //count newlines to print the last five lines
+        fs.readFile(file, function(err, data){
+                if (err) throw err;
+                done(data.split('\n').slice(-5).join('\n'));
+        })
+}
+
+function sort (file, done){
+        fs.readFile(file, function(err, text){
+                if (err) throw err;
+                done(text.split('\n').sort().join('\n'));
+        });
+}
+
+function wc (file, done){
+        fs.readFile(file, function(err, text){
+                if (err) throw err;
+                done(text.split('\n').length);
+        });
+}
+
+function uniq (file, done){
+        fs.readFile(file, function(err, text){
+                if (err) throw err;
+                var lines = text.split('\n');
+                for (var i = 0; i < lines.length; i++){
+                        if (lines[i] === lines[i + 1]){
+                                lines.splice(i, 1);
+                                i--;
+                        }
+                }
+                done(lines.join('\n'));
+        });
 }
 
 module.exports = {
@@ -50,5 +88,9 @@ module.exports = {
     ls: ls,
     echo: echo,
     cat: cat,
-    head:head
+    head: head,
+    tail: tail,
+    sort: sort,
+    wc: wc,
+    uniq: uniq
 }
